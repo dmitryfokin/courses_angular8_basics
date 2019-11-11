2fced94b523a0de946e5294f59d054953f72b12e
dkoderdvl <dkoder.dvl@gmail.com>
2019-11-11 13:15:50 +0300

    binding []

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-basics';
+
+  title = 'Dynamic title';
+  number = 42;
+  arr = [1, 2, 3];
+  obj = {
+    a: 1,
+    b: {c: 2}
+  };
+
+  obj$ = JSON.stringify(this.obj);
+
+  img = 'https://cdn.imgbin.com/14/10/23/imgbin-react-computer-icons-redux-javascript-others-sys1f0s7BFP7wbYz2H77apPRH.jpg';
+
+  constructor() {
+    setTimeout(() => {
+      console.log('5 seconds');
+      this.img = 'https://im0-tub-ru.yandex.net/i?id=d7fc7d45dd62b86fc50553ecc0962fbc&n=13';
+    }, 5000)
+  }
+
 }
