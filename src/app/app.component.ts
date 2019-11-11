7b337e9268dbd4045bc6decd383bb3c4c73b341f
dkoderdvl <dkoder.dvl@gmail.com>
2019-11-11 14:31:09 +0300

    binding ()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,15 +15,27 @@ export class AppComponent {
     b: {c: 2}
   };
 
-  obj$ = JSON.stringify(this.obj);
+  // obj$ = JSON.stringify(this.obj);
 
-  img = 'https://cdn.imgbin.com/14/10/23/imgbin-react-computer-icons-redux-javascript-others-sys1f0s7BFP7wbYz2H77apPRH.jpg';
+  // img = 'https://cdn.imgbin.com/14/10/23/imgbin-react-computer-icons-redux-javascript-others-sys1f0s7BFP7wbYz2H77apPRH.jpg';
+
+  inputValue: string = ''
+
+
 
   constructor() {
-    setTimeout(() => {
-      console.log('5 seconds');
-      this.img = 'https://im0-tub-ru.yandex.net/i?id=d7fc7d45dd62b86fc50553ecc0962fbc&n=13';
-    }, 5000)
-  }
+  //   setTimeout(() => {
+  //     console.log('5 seconds');
+  //     this.img = 'https://im0-tub-ru.yandex.net/i?id=d7fc7d45dd62b86fc50553ecc0962fbc&n=13';
+  //   }, 5000)
+   }
 
+   onInput(event) {
+     console.log(event);
+     this.inputValue = event.target.value;
+   }
+   onClick(event) {
+     console.log(event);
+     this.inputValue =this.inputValue.toUpperCase()
+   }
 }
